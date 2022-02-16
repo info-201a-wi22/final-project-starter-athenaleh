286dfd2fdd6da878a804bdc6e5472619fe3e0d7d
athenaleh <athenaleh@gmail.com>
2022-02-16 14:08:18 -0800

    scatterplot code update

--- source/chart_1.R
+++ source/chart_1.R
@@ -1,1 +1,22 @@
-athena
+# libraries
+library(ggplot2)
+
+# data simulation
+x <- 
+y <- 
+  
+# data frame
+data <- data.frame(x, y, group = disorders)
+
+# scatter plot by group 
+ggplot(data, aes(x = x, y = x, color = disorders)) +
+  geom_point() 
+
+# change look of points: geom_point(shape = number, size = number)
+
+# change shape per group 
+# ggplot(data, aes(x = x, y = y, color = group, shape = group))
+
+# legend customization
+# guides(color = guide_legend(title = "Title"))
+# custom key labels -- scale_color_discrete(labels = c("G1", "G2", "G3"))
