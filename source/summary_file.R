acc5d36441554478676bfe156d7b15a0e56076fe
athenaleh <athenaleh@gmail.com>
2022-02-18 10:03:48 -0800

    cleaner code

--- source/summary_file.R
+++ source/summary_file.R
@@ -17,10 +17,16 @@ highest_region
 most_prevelant_problem <- mental_health %>%
   filter(Year == max(Year))%>%
   group_by(Year)%>%
-  summarize(scizophrenia_avg = mean(Scizophrenia), bipolar_avg = mean(`Bipolar Disorders`), anxiety_avg = mean(`Anxiety Disroders`), drug_avg = mean(`Drug Use Disorders`), depressive_avg = mean(`Depressive Disorders`), alcohol_avg = mean(`Alcohol Use Disorders`))%>%
+  summarize(scizophrenia_avg = mean(Scizophrenia), 
+            bipolar_avg = mean(`Bipolar Disorders`), 
+            anxiety_avg = mean(`Anxiety Disroders`), 
+            drug_avg = mean(`Drug Use Disorders`), 
+            depressive_avg = mean(`Depressive Disorders`), 
+            alcohol_avg = mean(`Alcohol Use Disorders`)) %>%
   select(scizophrenia_avg, bipolar_avg, anxiety_avg, drug_avg, depressive_avg, alcohol_avg)
  most_prevelant_problem 
-# Which country has the lowest amount of mental health issues?
+
+ # Which country has the lowest amount of mental health issues?
 # ("may be skewed/ biased (see below). If we delve into this country specifically, we can 
 # extrapolate lessons on how governments can minimize mental illnesses to better serve their citizens)
 
@@ -31,5 +37,7 @@ lowest_prevalence <- mental_health_gov %>%
   filter(`Regime Mental Health Avg` == min(`Regime Mental Health Avg`))%>%
   pull(Regime)
 lowest_prevalence
+
 # What is the correlation between unemployment rates and the prevalence of mental illnesses?
 # (calculate a correlation coefficient - like a Pearson's R or p-value)
+
