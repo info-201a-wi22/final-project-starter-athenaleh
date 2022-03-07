944eb480b229e1baa8934f837542b4a3f7bef9ff
athenaleh <athenaleh@gmail.com>
2022-03-06 18:24:14 -0800

    Athena update 2

--- docs/app_ui.R
+++ docs/app_ui.R
@@ -1,5 +1,5 @@
 # app_ui 
-source("summary_file.R")
+source("../source/summary_file.R")
 most_prevelant_problem #currently
 lowest_region #recently
 lowest_prevalence #government currently. 
@@ -41,14 +41,14 @@ ui <- fluidPage(
            p("health. In doing so we hoped to get the tools to provide regions with poor mental health"),
            p("to better the lives of their people. To do so, we needed to answer a few questions that would"),
            p("help in doing so. The first question we asked was what is the current most prevelant mental health"),
-           p("problem. It turns out that the answer to that is", lowest_prevelance, ". We then wanted to know"),
-           p("What region currently has the lowest mental health issues. The answer turned out to be", lowest_region,),
+           p("problem. It turns out that the answer to that is", em(most_prevalent_problem), ". We then wanted to know"),
+           p("What region currently has the lowest mental health issues. The answer turned out to be", em(lowest_region,),
            p("Finally, we also wanted to know which government type(s) has the lowest prevelance of mental health"),
-           p("issues. It turned out that", lowest_prevalence, "have the lowest prevelance. In finding the answers to"),
+           p("issues. It turned out that", em(most_prevalent_problem), "have the lowest prevelance. In finding the answers to"),
            p("these questions, we can not only find out what government type and region is thriving mentally, but we can"),
            p("then pull from their success and implement them into regions that are not doing well mentally.")
            ),
   tabPanel("Report Page",
            p(""))
       )
-)
+))
