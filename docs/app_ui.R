19ed4f90e221277b942f0e0ff3b77655dcb1a6bc
rlevy123 <97078684+rlevy123@users.noreply.github.com>
2022-03-06 17:19:35 -0800

    summary done

--- docs/app_ui.R
+++ docs/app_ui.R
@@ -1,6 +1,8 @@
 # app_ui 
-
-
+source("summary_file.R")
+most_prevelant_problem #currently
+lowest_region #recently
+lowest_prevalence #government currently. 
 ui <- fluidPage(
   titlePanel("Mental Health Issues Across the World"),
   tabsetPanel(
@@ -26,21 +28,23 @@ ui <- fluidPage(
              p("")
            )),
   tabPanel("Summary",
-             p("")
-<<<<<<< HEAD
-           )
-      )
-=======
-           ),
+             p("In this project, our main goal was to get a better understanding in regards to mental"),
+           p("health. In doing so we hoped to get the tools to provide regions with poor mental health"),
+           p("to better the lives of their people. To do so, we needed to answer a few questions that would"),
+           p("help in doing so. The first question we asked was what is the current most prevelant mental health"),
+           p("problem. It turns out that the answer to that is", lowest_prevelance, ". We then wanted to know"),
+           p("What region currently has the lowest mental health issues. The answer turned out to be", lowest_region,),
+           p("Finally, we also wanted to know which government type(s) has the lowest prevelance of mental health"),
+           p("issues. It turned out that", lowest_prevalence, "have the lowest prevelance. In finding the answers to"),
+           p("these questions, we can not only find out what government type and region is thriving mentally, but we can"),
+           p("then pull from their success and implement them into regions that are not doing well mentally.")
+           )),
   tabPanel("Report Page",
            p(""))
       )
-)
-<<<<<<< HEAD
+
+
 
 
   
-=======
-  
->>>>>>> c3bcbc948c2605c1413eb772e46ae01e1d71d415
->>>>>>> 8fc47c36afde81678263fb7805a27844628ae1ce
+
